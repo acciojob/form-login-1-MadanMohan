3de50d60541dbe0f53b3898cbf1fe085a47bbda0
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-11 17:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementsByName("fname").value;
+	const lname = document.getElementsByName("lname").value;
 
+		alert(fname+ " "+lname);
 }
+
